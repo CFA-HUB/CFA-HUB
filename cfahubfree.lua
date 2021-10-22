21a46986f6b78089d0b90ac202a059036a7be961
CFA HUB <90056748+CFA-HUB@users.noreply.github.com>
2021-10-22 22:53:23 +0700

    Update cfahubfree.lua

--- cfahubfree.lua
+++ cfahubfree.lua
@@ -1,10 +1,6 @@
 if game.PlaceId==2753915549 or game.PlaceId==4442272183 or game.PlaceId==7449423635 then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/CFA-HUB/CFA-HUB/main/bloxfruit.lua"))()
 end
-if game.PlaceId==6874226845 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CFA-HUB/CFA-HUB/main/notfruit.lua"))()
-
-end
 if game.PlaceId==6284583030 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CFA-HUB/CFA-HUB/main/petx.lua"))()
 end
