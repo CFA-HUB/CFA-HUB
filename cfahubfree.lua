58501b8ff0222e7c019448c7e5fc56398994176b
CFA HUB <90056748+CFA-HUB@users.noreply.github.com>
2022-07-12 18:52:35 +0700

    Update cfahubfree.lua

--- cfahubfree.lua
+++ cfahubfree.lua
@@ -5,5 +5,5 @@ if game.PlaceId==6284583030 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/CFA-HUB/CFA-HUB/main/petx.lua"))()
 end
 if game.PlaceId==3978370137 then 
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/CFA-HUB/CFAHub-Free/main/gpofreecfahub.lua))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/CFA-HUB/CFAHub-Free/main/gpofreecfahub.lua"))()
 end
