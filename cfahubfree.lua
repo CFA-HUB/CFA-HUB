9f13cdbba9c802df72cc095bed922ee6a7566988
CFA HUB <90056748+CFA-HUB@users.noreply.github.com>
2023-05-30 09:18:39 +0700

    Update cfahubfree.lua

--- cfahubfree.lua
+++ cfahubfree.lua
@@ -7,7 +7,7 @@ end
 if game.PlaceId==3978370137 or game.PlaceId==1730877806 then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/CFA-HUB/CFAHub-Free/main/gpofreecfahub.lua"))()
 end
-if game.PlaceId == 10464237910 then 
+if game.PlaceId == 10464237910 or game.PlaceId == 13373351184 then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/CFA-HUB/CFAHub-Free/main/DragonSea.lua"))()
 
 end
